3e2a01339ca8b2400bbbdb479d481c189eeab5b6
EN0M1 <eli.mizerski@gmail.com>
2025-02-05 15:31:14 -0600

    Fixed BallBehavior and TimerBehavior
    Fixed BallBehavior and TimerBehavior, still need to add pin behavior and catch up to date with current material (as of Feb 5th)

--- Assets/Scripts/BallBehavior.cs
+++ Assets/Scripts/BallBehavior.cs
@@ -11,43 +11,79 @@ public class BallBehavior : MonoBehaviour
     public float maxSpeed;
     Vector2 targetPosition;
 
-    public int secondsToMaxSpeed;
+    public GameObject target;
+    public bool launching;
+    public float timeLastLaunch;
+    public float launchDuration;
+    public float timeLaunchStart;
+    public float minLaunchSpeed;
+    public float maxLaunchSpeed;
+    public float cooldown;
 
+    public int secondsToMaxSpeed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // secondsToMaxSpeed = 30;
-        // minSpeed = 0.00001f;
-        // maxSpeed = 0.75f;
         targetPosition = getRandomPosition();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (onCooldown() == false)
+        {
+            if (launching == true)
+            {
+                float currentLaunchTime = Time.time - timeLaunchStart;
+                if (currentLaunchTime > launchDuration) 
+                {
+                    startCooldown();
+                }
+            }
+            else
+            {
+                Debug.Log("unim");
+                launch();
+            }
+        }
         Vector2 currentPosition = gameObject.GetComponent<Transform>().position;
         float distance = Vector2.Distance(currentPosition, targetPosition);
-        if (distance > 0.1f)
+        if (distance > 0.1)
         {
             float difficulty = getDifficultyPercentage();
-            float currentSpeed = Mathf.Lerp(minSpeed, maxSpeed, difficulty);
+            float currentSpeed;
+            if (launching == true)
+            {
+                float launchingForHowLong = Time.time - timeLaunchStart;
+                if (launchingForHowLong > launchDuration)
+                {
+                    startCooldown();
+                }
+                currentSpeed = Mathf.Lerp(minLaunchSpeed, maxLaunchSpeed, difficulty);
+            }
+            else
+            {
+                currentSpeed = Mathf.Lerp(minSpeed, maxSpeed, difficulty);
+            }
             currentSpeed = currentSpeed * Time.deltaTime;
             Vector2 newPosition = Vector2.MoveTowards(currentPosition, targetPosition, currentSpeed);
             transform.position = newPosition;
         }
         else
         {
+            if (launching == true)
+            {
+                startCooldown();
+            }
             targetPosition = getRandomPosition();
         }
-        getRandomPosition();
     }
 
     Vector2 getRandomPosition()
     {   
         float randX = Random.Range(minX, maxX);
         float randY = Random.Range(minY, maxY);
-        Debug.Log("rx: " + randX + "ry: " + randY);
         Vector2 v = new Vector2(randX, randY);
         return v;
     }
@@ -55,9 +91,33 @@ public class BallBehavior : MonoBehaviour
     private float getDifficultyPercentage() 
     {
         float difficulty = Mathf.Clamp01(Time.timeSinceLevelLoad / secondsToMaxSpeed);
-
         return difficulty;
     }
 
-    
+    public void launch() 
+    {
+        targetPosition = target.transform.position;
+        if (launching == false) 
+        {
+            timeLaunchStart = Time.time;
+            launching = true;
+        }
+    }
+
+    public bool onCooldown() 
+    {
+        bool result = false;
+        float timeSinceLastLaunch = (Time.time - timeLastLaunch);
+        if (timeSinceLastLaunch > cooldown)
+        {
+            result = true;
+        }
+        return result;
+    }
+
+    void startCooldown()
+    {
+        timeLastLaunch = Time.time;
+        launching = false;
+    }
 }
