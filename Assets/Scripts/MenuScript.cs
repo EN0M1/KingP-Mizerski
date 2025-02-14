77615349f85331d7d266bc66bb7a4cd34e2faa4c
EN0M1 <eli.mizerski@gmail.com>
2025-02-14 16:00:38 -0600

    Added Menus
    Added builds and functionality for the four scenes, main menu, main game, character selection, and game over

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -4,6 +4,16 @@ public class MenuScript : MonoBehaviour
 {
     public void GoToGame()
     {
-        
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainGame");
+    }
+
+    public void GoToMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+
+    public void GoToCharacterSelection()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("CharacterSelection");
     }
 }
